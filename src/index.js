2715989a85f2361fba94a9a91316d2154c0d54e0
GentleReaper <cyraxmuia@gmail.com>
2025-03-16 22:35:22 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const taskDescription = document.getElementById('new-task-description').value;
+
+      if (taskDescription.trim() !== "") {
+          const li = document.createElement('li');
+          li.textContent = taskDescription;
+
+          const deleteButton = document.createElement('button');
+          deleteButton.textContent = 'Delete';
+          deleteButton.addEventListener('click', function() {
+              li.remove();
+          });
+          taskList.appendChild(li);
+          document.getElementById('new-task-description').value = '';
+      }
+  });
 });
